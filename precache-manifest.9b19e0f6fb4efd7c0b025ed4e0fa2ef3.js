1260a3e5752867a8624d11f840c8a94c96affce1
Rikitariko <leoprogbars@gmail.com>
2020-09-09 22:14:47 +0300

    Updates

---
+++ precache-manifest.9b19e0f6fb4efd7c0b025ed4e0fa2ef3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "83478d265a0d0a05cac8",
+    "url": "/first-app/static/css/main.bff31c88.chunk.css"
+  },
+  {
+    "revision": "83478d265a0d0a05cac8",
+    "url": "/first-app/static/js/main.3c57a79c.chunk.js"
+  },
+  {
+    "revision": "9bc2a1921ac1eff84df1",
+    "url": "/first-app/static/js/runtime~main.ada60267.js"
+  },
+  {
+    "revision": "dcc9ff57896a7b7d3e0a",
+    "url": "/first-app/static/js/2.9e845cfc.chunk.js"
+  },
+  {
+    "revision": "6ed110062c75348740e903ff4f05ff0e",
+    "url": "/first-app/static/media/loginIcon.6ed11006.png"
+  },
+  {
+    "revision": "105ff364333bf3d0284f2593c94d3681",
+    "url": "/first-app/static/media/icon.105ff364.svg"
+  },
+  {
+    "revision": "4edd1b95cb86de15e0454d00e1ef8cd6",
+    "url": "/first-app/static/media/logo.4edd1b95.png"
+  },
+  {
+    "revision": "e0d0f37c41ef9d92d0c186cdfa61c21b",
+    "url": "/first-app/static/media/record.e0d0f37c.svg"
+  },
+  {
+    "revision": "bf1f1f14dffd9b633788e0c0a3fd2483",
+    "url": "/first-app/index.html"
+  }
+];
